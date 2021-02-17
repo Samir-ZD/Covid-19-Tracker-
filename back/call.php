07d3a84e3ec8fd96fbecba6010a577863698b582
Samir Zahreddine <73433976+Samir-ZD@users.noreply.github.com>
2021-02-17 05:12:35 +0200

    Create call.php
    establishing Database Connection

---
+++ back/call.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "your server name";
+$username = "your username";
+$password = "your DB password";
+$dbname = "your DB name";
+
+// Create connection:
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection:
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
